d12b9e63b874efb0a1dc074084c3a6084a0762e6
Rifqialfaizi <rifqialfaizii@gmail.com>
2019-01-28 21:32:14 +0700

    Initial Commit

---
+++ MoviesApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesApps
+//
+//  Created by Rifqi Alfaizi on 28/01/19.
+//  Copyright © 2019 Rifqi Alfaizi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
